66882cfb62fb1fc6b6419f60acca868935b275d9
Yatin Korgaonkar <korgaonkaryatin09@gmail.com>
2022-10-06 21:08:20 +0530

    Added the brand theme

---
+++ src/EazyTheme.jsx
@@ -0,0 +1,27 @@
+const EazyTheme = {
+  colors: {
+    brand: [
+      "#F5F5F5",
+      "#D8D8D9",
+      "#BFC0C0",
+      "#A7AAAC",
+      "#91969B",
+      "#7B858F",
+      "#647687",
+      "#5E666F",
+      "#55595D",
+      "#4B4D4F"
+    ]
+  },
+  colorScheme: "light",
+  fontFamily: "Poppins, sans-serif",
+  fontFamilyMonospace: "Monaco, Courier, monospace",
+  headings: { fontFamily: "Merriweather, serif" },
+  focusRing: "auto",
+  primaryColor: "brand",
+  defaultRadius: "md",
+  activeStyles: "none",
+  loader: "oval"
+};
+
+export default EazyTheme;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import EazyTheme from "./EazyTheme";
 import App from "./App";
+import "@fontsource/poppins";
+import "@fontsource/merriweather";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={EazyTheme} withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
